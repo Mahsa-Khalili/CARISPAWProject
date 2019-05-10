2c674bfc86623847facf5e3213002a5ab89f1f3c
Kevin Ta <kevin.jhy.ta@gmail.com>
2019-05-10 11:50:37 -0700

    PAW002 - Adjusted code so that real-time data collection is working, although the current set-up has poorly defined windows. Moved saving data from main loop so opening, closing, and writing the file is not taking up processing time.

--- WheelDataAcqusition.py
+++ WheelDataAcqusition.py
@@ -9,32 +9,110 @@ Purpose:        To provide a visualization for the data acquisition process.
 import os
 import datetime
 import time
-import imp
+import numpy as np
 import csv
 import struct
 import bluetooth
 from cobs import cobs
+from libraries.imumsg import imumsg_pb2 as imuMsg
+from libraries import realtimelib
+from vispy import gloo
+from vispy import app
+import math
+import threading
 
 # DEFINITIONS
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-rightIMUDataFilePath = os.path.join(dir_path, 'IMU Data', '%s_rightIMUMessage.csv' % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-leftIMUDataFilePath = os.path.join(dir_path, 'IMU Data', '%s_leftIMUMessage.csv' % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+rightIMUDataFilePath = os.path.join('IMU Data', '{} rightIMUMessage.csv'.format(datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")))
+leftIMUDataFilePath = os.path.join('IMU Data', '{} leftIMUMessage.csv'.format(datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")))
 
-imuMsgPath = os.path.join(dir_path, 'release', 'imumsg_pb2.py')
-print(imuMsgPath)
-imuMsg = imp.load_source("imumsg_pb2",imuMsgPath)
+
+# imuMsgPath = os.path.join(dir_path, 'libraries', 'imumsg_pb2.py')
+# print(imuMsgPath)
+# imuMsg = importlib.import_module('imumsg_pb2', imuMsgPath)
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-bluetoothHost = 1
-bluetoothPort = 2
 RaspberryPiAddress = "B8:27:EB:6B:15:7F"
+
+displayData = np.zeros((6, 1000)).astype(np.float32)
 
 Left = {'Address': "98:D3:51:FD:AD:F5", "DataPath": leftIMUDataFilePath}
 Right = {'Address': "98:D3:81:FD:48:C9", "DataPath": rightIMUDataFilePath}
 
 # CLASSES
+
+class ClUIWrapper():
+
+    def __init__(self, Source):
+        self.loop = ClWheelDataParsing(Source)
+        self.canvas = Canvas()
+        self.source = Source
+
+    def fnStart(self):
+
+        threads = []
+
+        # p2 = threading.Thread(target=app.run)
+        # threads.append(p2)
+        # p2.start()
+        p1 = threading.Thread(target=self.loop.fnRun)
+        threads.append(p1)
+        p1.start()
+
+        # self.loop.fnRun()
+        app.run()
+
+        np.savetxt(self.source['DataPath'], np.transpose([self.loop.xData, self.loop.yData, self.loop.zData, self.loop.xGyro,
+                                             self.loop.yGyro, self.loop.zGyro]), delimiter=",")
+
+
+class Canvas(app.Canvas):
+    def __init__(self):
+        app.Canvas.__init__(self, title='Use your wheel to zoom!',
+                            keys='interactive')
+        self.program = gloo.Program(realtimelib.VERT_SHADER, realtimelib.FRAG_SHADER)
+        self.program['a_position'] = displayData.reshape(-1, 1)
+        self.program['a_color'] = realtimelib.color
+        self.program['a_index'] = realtimelib.index
+        self.program['u_scale'] = (1., 1.)
+        self.program['u_size'] = (realtimelib.nrows, realtimelib.ncols)
+        self.program['u_n'] = realtimelib.n
+
+        gloo.set_viewport(0, 0, *self.physical_size)
+
+        self._timer = app.Timer('auto', connect=self.on_timer, start=True)
+
+        gloo.set_state(clear_color='black', blend=True,
+                       blend_func=('src_alpha', 'one_minus_src_alpha'))
+
+        self.show()
+
+    def on_resize(self, event):
+        gloo.set_viewport(0, 0, *event.physical_size)
+
+    def on_mouse_wheel(self, event):
+        dx = np.sign(event.delta[1]) * .05
+        scale_x, scale_y = self.program['u_scale']
+        scale_x_new, scale_y_new = (scale_x * math.exp(2.5*dx),
+                                    scale_y * math.exp(0.0*dx))
+        self.program['u_scale'] = (max(1, scale_x_new), max(1, scale_y_new))
+        self.update()
+
+    def on_timer(self, event):
+        """Add some data at the end of each signal (real-time signals)."""
+        k = 10
+
+        print('Running canvas.')
+
+        self.program['a_position'].set_data(displayData.ravel().astype(np.float32))
+        self.update()
+
+    def on_draw(self, event):
+        gloo.clear()
+        self.program.draw('line_strip')
+
 
 class ClWheelDataParsing:
     """
@@ -57,9 +135,7 @@ class ClWheelDataParsing:
         self.yGyro = []
         self.zGyro = []
 
-        self.IMUStorage = open(source['DataPath'], mode='w')
-        self.IMUCSV = csv.writer(self.IMUStorage)
-
+        self.IMUStorage = source['DataPath']
 
     def fnRun(self):
         """
@@ -68,24 +144,15 @@ class ClWheelDataParsing:
 
         """
 
+        status = 'Active.'
+
         self.IMU.COBSIntialClear()
 
-        while True:
-            data = []
-            c = self.IMU.sock.recv(1)
-            if c == b'':
-                self.IMU.onDisconnect()
-                break
-            while c != b'\x00' and c != b'':
-                data.append(c)
-                c = self.IMU.sock.recv(1)
-            data = b''.join(data)
-            try:
-                IMUMessage = self.IMU.decodeCOBS(data)
-            except Exception as e:
-                print("Failed to decode message.")
-
-            self.fnReceiveData(IMUMessage)
+        while status != 'Disconnected.':
+            status = self.IMU.fnRetieveIMUMessage()
+            # print("Message retrieval complete.")
+            self.fnReceiveData(self.IMU.cobsMessage)
+            # print('Running wheel module.')
 
         self.IMU.sock.close()
 
@@ -96,15 +163,15 @@ class ClWheelDataParsing:
 
         dataSizeArray = msg[:4]
         dataSize = struct.unpack("<L", dataSizeArray)[0]
-        print(len(msg))
-        print(dataSize)
+        # print(len(msg))
+        # print(dataSize)
         data = msg[4:]
         imuMsgBT = imuMsg.IMUInfo()
         imuMsgBT.ParseFromString(data)
 
-        print("Value In onRecieveData: %f" % imuMsgBT.acc_x)
-        print("Value: %f" % imuMsgBT.acc_y)
-        print("Data from sensor " + imuMsgBT.sensorID)
+        # print("Value In onRecieveData: %f" % imuMsgBT.acc_x)
+        # print("Value: %f" % imuMsgBT.acc_y)
+        # print("Data from sensor " + imuMsgBT.sensorID)
 
         self.fnStoreData(imuMsgBT.acc_x, imuMsgBT.acc_y, imuMsgBT.acc_z, imuMsgBT.angular_x, imuMsgBT.angular_y, imuMsgBT.angular_z)
 
@@ -112,8 +179,11 @@ class ClWheelDataParsing:
         """
         Stores data into CSV storage.
         """
-
-        self.IMUCSV.writerow([xAcc, yAcc, zAcc, xGyro, yGyro, zGyro])
+        # with open(self.IMUStorage, mode='a') as csvfile:
+        #     writer = csv.writer(csvfile)
+        #     writer.writerow([xAcc, yAcc, zAcc, xGyro, yGyro, zGyro])
+        displayData[:,:] = np.roll(displayData, -1)
+        displayData[0:6, -1] = [xAcc, yAcc, zAcc, xGyro, yGyro, zGyro]
         self.xData.append(xAcc)
         self.yData.append(yAcc)
         self.zData.append(zAcc)
@@ -122,44 +192,11 @@ class ClWheelDataParsing:
         self.zGyro.append(zGyro)
 
 
-class ClPAWDataStream:
-    """
-    Connect to HC05 Bluetooth module.
-    """
-
-    def __init__(self):
-        """
-        Creates data parsing instances.
-        Feeds data parsing instances to data client.
-        """
-        self.IMUInstance = ClWheelDataParsing()
-        self.dataClientRight = ClDataTransferClient(HC05RightAddress)
-        self.dataClientLeft = ClDataTransferClient(HC05LeftAddress)
-
-    def fnRun(self):
-
-        while True:
-            self.dataClientRight.fnRun()
-            self.dataClientLeft.fnRun()
-            self.fnDisplayData("right")
-            self.fnDisplayData("left")
-            time.sleep(0.5)
-
-    def fnDisplayData(self,  sensorType):
-
-        if sensorType == "right":
-            self.rightHC05PlotXAxis(self.IMUInstance.xRightData, self.IMUInstance.yRightData, self.IMUInstance.zRightData,
-                                    self.IMUInstance.xRightGyro, self.IMUInstance.yRightGyro, self.IMUInstance.zRightGyro,'r-')
-
-        elif sensorType == "left":
-            self.leftHC05PlotXAxis(self.IMUInstance.xLeftData, self.IMUInstance.yLeftData, self.IMUInstance.zLeftData,
-                                   self.IMUInstance.xLeftGyro, self.IMUInstance.yLeftGyro, self.IMUInstance.zLeftGyro,'r-')
-
 class ClBluetoothConnect:
 
     def __init__(self, BTAddress):
 
-        self.cobsmsg = ''
+        self.cobsMessage = ''
         self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
         print ("%s: Began connection" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
@@ -168,24 +205,27 @@ class ClBluetoothConnect:
 
         print ("%s: Established connection" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+
     def fnRetieveIMUMessage(self):
 
         data = []
         c = self.sock.recv(1)
-        if c == b'':
-            self.IMU.onDisconnect()
-            break
-        while c != b'\x00' and c != b'':
+
+        while c != b'\x00':
+            if c == b'':
+                self.onDisconnect()
+                return "Disconnected."
             data.append(c)
-            c = self.IMU.sock.recv(1)
+            c = self.sock.recv(1)
         data = b''.join(data)
         try:
-            IMUMessage = self.IMU.decodeCOBS(data)
+            self.cobsMessage = self.decodeCOBS(data)
+            return "Received."
         except Exception as e:
-            print("Failed to decode message (SocketHandler)")
+            print("Failed to decode message due to {}".format(e))
 
     def onDisconnect(self):
-        print("Disconnected from server")
+        print("Disconnected from server.")
         self.shutDown()
 
     def decodeCOBS(self, encodedCobsMsg):
@@ -223,10 +263,14 @@ class ClBluetoothConnect:
 
             chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
-        return ''.join(chunks)
+        return b''.join(chunks)
 
 
 if __name__ == "__main__":
-    loop = ClWheelDataParsing()
+    # loop = ClWheelDataParsing(Left)
+    # loop.fnRun()
+    UIWrap = ClUIWrapper(Right)
+    UIWrap.fnStart()
+
     name = input('What is your name? \n')
     pass
